c7ce5b92e446868821540976089b193279faabb1
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-11-23 16:06:48 +0100

    changed precheck added filter for BlackCat CMS

--- install.php
+++ install.php
@@ -5,8 +5,8 @@
  *
  * @author Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
  * @link http://phpmanufaktur.de
- * @copyright 2009 - 2012
- * @license http://www.gnu.org/licenses/gpl.html GNU Public License (GPL)
+ * @copyright 2009 - 2013
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
  */
 
 // include class.secure.php to protect this file and the whole CMS!
@@ -29,110 +29,118 @@ if (defined('WB_PATH')) {
 // end include class.secure.php
 
 if (WB_VERSION < 2.8) {
-	$message = 'Sorry, but dbGlossary needs Website Baker 2.8, the installation was NOT successfull! Please UNINSTALL dbGlossary to prevent problems!';
-	echo '<script language="javascript">alert ("'.$message.'");</script>';
+    $message = 'Sorry, but dbGlossary needs Website Baker 2.8, the installation was NOT successfull! Please UNINSTALL dbGlossary to prevent problems!';
+    echo '<script language="javascript">alert ("'.$message.'");</script>';
 }
 else {
 
-	require_once(WB_PATH.'/modules/'.basename(dirname(__FILE__)).'/class.glossary.php');
-	require_once(WB_PATH.'/modules/'.basename(dirname(__FILE__)).'/class.droplets.php');
+    require_once(WB_PATH.'/modules/'.basename(dirname(__FILE__)).'/class.glossary.php');
+    require_once(WB_PATH.'/modules/'.basename(dirname(__FILE__)).'/class.droplets.php');
 
-	global $admin;
+    global $admin;
 
-	$error = '';
-	// Install dbGlossary
-	$dbGlossary = new dbGlossary(true);
-	if ($dbGlossary->isError()) {
-		$error .= sprintf('<p>[Installation] %s</p>', $dbGlossary->getError());
-	}
-	// Install dbGlossaryLiterature
-	$dbGlossaryLiterature = new dbGlossaryLiterature(true);
-	if ($dbGlossaryLiterature->isError()) {
-		$error .= sprintf('<p>[Installation] %s</p>', $dbGlossaryLiterature->getError());
-	}
-	// Install dbGlossaryFootnotes
-	$dbGlossaryFootnotes = new dbGlossaryFootnotes(true);
-	if ($dbGlossaryFootnotes->isError()) {
-		$error .= sprintf('<p>[Installation] %s</p>', $dbGlossaryFootnotes->getError());
-	}
-	// Install dbGlossaryCfg
-	$dbGlossaryCfg = new dbGlossaryCfg(true);
-	if ($dbGlossaryCfg->isError()) {
-		$error .= sprintf('<p>[Installation] %s</p>', $dbGlossaryCfg->getError());
-	}
+    $error = '';
+    // Install dbGlossary
+    $dbGlossary = new dbGlossary(true);
+    if ($dbGlossary->isError()) {
+        $error .= sprintf('<p>[Installation] %s</p>', $dbGlossary->getError());
+    }
+    // Install dbGlossaryLiterature
+    $dbGlossaryLiterature = new dbGlossaryLiterature(true);
+    if ($dbGlossaryLiterature->isError()) {
+        $error .= sprintf('<p>[Installation] %s</p>', $dbGlossaryLiterature->getError());
+    }
+    // Install dbGlossaryFootnotes
+    $dbGlossaryFootnotes = new dbGlossaryFootnotes(true);
+    if ($dbGlossaryFootnotes->isError()) {
+        $error .= sprintf('<p>[Installation] %s</p>', $dbGlossaryFootnotes->getError());
+    }
+    // Install dbGlossaryCfg
+    $dbGlossaryCfg = new dbGlossaryCfg(true);
+    if ($dbGlossaryCfg->isError()) {
+        $error .= sprintf('<p>[Installation] %s</p>', $dbGlossaryCfg->getError());
+    }
 
-	if (defined('LEPTON_VERSION')) {
-		// register imageTweak at LEPTON outputInterface
-		if (!file_exists(WB_PATH .'/modules/output_interface/output_interface.php')) {
-			$error .= '<p>Missing LEPTON outputInterface, can\'t register dbGlossary - installation is not complete!</p>';
-		}
-		else {
-			if (!function_exists('register_output_filter')) include_once(WB_PATH .'/modules/output_interface/output_interface.php');
-			register_output_filter('dbglossary', 'dbGlossary');
-		}
-	} // LEPTON
-	elseif (version_compare(WB_VERSION, '2.8.3', '>=')) {
-	  // Patch WebsiteBaker 2.8.3
-	  $message = "";
-	  if (file_exists(WB_PATH.'/modules/output_filter/index.php')) {
-	    if (!isPatched(WB_PATH.'/modules/output_filter/index.php')) {
-	      if (doPatchWB283(WB_PATH.'/modules/output_filter/index.php')) {
-	        $message = "Frontend output filter succesfully patched for dbGlossary... dbGlossary are now ready to use.";
-	      }
-	      else {
-	        $message = "Patching frontend output filter failed... Please click the HELP button in the dbGlossary admintool for instructions how to manual patch the output filter.";
-	      }
-	    }
-	    else {
-	      $message = "The frontend output filter was already patched.";
-	    }
-	  }
-	  else {
-	    $message = "Installation not completed. The frontend output filter does not exist (Wrong WB version?). dbGlossary cannot be used.";
-	  }
-	  if ($message != "") {
-	    echo '<script language="javascript">alert ("'.$message.'");</script>';
-	  }
-	}
-	else {
-		// Patch WebsiteBaker Output Filter
-		$message = "";
-		if (file_exists(WB_PATH .'/modules/output_filter/filter-routines.php')) {
-		  if (!isPatched(WB_PATH .'/modules/output_filter/filter-routines.php')) {
-				if (doPatch(WB_PATH .'/modules/output_filter/filter-routines.php')) {
-						$message = "Frontend output filter succesfully patched for dbGlossary... dbGlossary are now ready to use.";
-				}
-				else {
-					$message = "Patching frontend output filter failed... Please click the HELP button in the dbGlossary admintool for instructions how to manual patch the output filter.";
-				}
-			}
-			else {
-				$message = "The frontend output filter was already patched.";
-			}
-		}
-		else {
-			$message = "Installation not completed. The frontend output filter does not exist (Wrong WB version?). dbGlossary cannot be used.";
-		}
-		if ($message != "") {
-			echo '<script language="javascript">alert ("'.$message.'");</script>';
-		}
-	} // output_filter
+    if (defined('LEPTON_VERSION')) {
+        // register imageTweak at LEPTON outputInterface
+        if (!file_exists(WB_PATH .'/modules/output_interface/output_interface.php')) {
+            $error .= '<p>Missing LEPTON outputInterface, can\'t register dbGlossary - installation is not complete!</p>';
+        }
+        else {
+            if (!function_exists('register_output_filter')) include_once(WB_PATH .'/modules/output_interface/output_interface.php');
+            register_output_filter('dbglossary', 'dbGlossary');
+        }
+    } // LEPTON
+    elseif (defined('CAT_VERSION')) {
+        // register the filter at the blackcatFilter
+        require_once CAT_PATH.'/modules/blackcatFilter/filter.php';
+        // first unregister to prevent trouble at re-install
+        unregister_filter('dbGlossary', 'dbglossary');
+        // register the filter
+        register_filter('dbGlossary', 'dbglossary', 'Enable the dbGlossary content filter');
+    }
+    elseif (version_compare(WB_VERSION, '2.8.3', '>=')) {
+      // Patch WebsiteBaker 2.8.3
+      $message = "";
+      if (file_exists(WB_PATH.'/modules/output_filter/index.php')) {
+        if (!isPatched(WB_PATH.'/modules/output_filter/index.php')) {
+          if (doPatchWB283(WB_PATH.'/modules/output_filter/index.php')) {
+            $message = "Frontend output filter succesfully patched for dbGlossary... dbGlossary are now ready to use.";
+          }
+          else {
+            $message = "Patching frontend output filter failed... Please click the HELP button in the dbGlossary admintool for instructions how to manual patch the output filter.";
+          }
+        }
+        else {
+          $message = "The frontend output filter was already patched.";
+        }
+      }
+      else {
+        $message = "Installation not completed. The frontend output filter does not exist (Wrong WB version?). dbGlossary cannot be used.";
+      }
+      if ($message != "") {
+        echo '<script language="javascript">alert ("'.$message.'");</script>';
+      }
+    }
+    else {
+        // Patch WebsiteBaker Output Filter
+        $message = "";
+        if (file_exists(WB_PATH .'/modules/output_filter/filter-routines.php')) {
+          if (!isPatched(WB_PATH .'/modules/output_filter/filter-routines.php')) {
+                if (doPatch(WB_PATH .'/modules/output_filter/filter-routines.php')) {
+                        $message = "Frontend output filter succesfully patched for dbGlossary... dbGlossary are now ready to use.";
+                }
+                else {
+                    $message = "Patching frontend output filter failed... Please click the HELP button in the dbGlossary admintool for instructions how to manual patch the output filter.";
+                }
+            }
+            else {
+                $message = "The frontend output filter was already patched.";
+            }
+        }
+        else {
+            $message = "Installation not completed. The frontend output filter does not exist (Wrong WB version?). dbGlossary cannot be used.";
+        }
+        if ($message != "") {
+            echo '<script language="javascript">alert ("'.$message.'");</script>';
+        }
+    } // output_filter
 
-	// Install Droplets
-	$droplets = new checkDroplets();
+    // Install Droplets
+    $droplets = new checkDroplets();
   if ($droplets->insertDropletsIntoTable()) {
-  	$message = 'The Droplets for dbGlossary where successfully installed! Please look at the Help for further informations.';
+      $message = 'The Droplets for dbGlossary where successfully installed! Please look at the Help for further informations.';
   }
   else {
-  	$message = 'The installation of the Droplets for dbGlossary failed. Error: '. $droplets->getError();
+      $message = 'The installation of the Droplets for dbGlossary failed. Error: '. $droplets->getError();
   }
   if ($message != "") {
-		echo '<script language="javascript">alert ("'.$message.'");</script>';
-	}
+        echo '<script language="javascript">alert ("'.$message.'");</script>';
+    }
 
-	if (!empty($error)) {
-		$admin->print_error($error);
-	}
+    if (!empty($error)) {
+        $admin->print_error($error);
+    }
 
 } // WB 2.8
 
--- precheck.php
+++ precheck.php
@@ -5,8 +5,8 @@
  *
  * @author Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
  * @link http://phpmanufaktur.de
- * @copyright 2009 - 2012
- * @license http://www.gnu.org/licenses/gpl.html GNU Public License (GPL)
+ * @copyright 2009 - 2013
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
  */
 
 // include class.secure.php to protect this file and the whole CMS!
@@ -33,19 +33,18 @@ if (defined('WB_PATH')) {
 if (!defined('LEPTON_VERSION'))
   $PRECHECK['WB_VERSION'] = array('VERSION' => '2.8', 'OPERATOR' => '>=');
 $PRECHECK['PHP_VERSION'] = array('VERSION' => '5.2.0', 'OPERATOR' => '>=');
-$PRECHECK['WB_ADDONS'] = array(
-	'dbconnect_le'	=> array('VERSION' => '0.69', 'OPERATOR' => '>='),
-	'rhtools' => array('VERSION' => '0.51', 'OPERATOR' => '>='),
-	'dwoo' => array('VERSION' => '0.13', 'OPERATOR' => '>=')
-);
+$PRECHECK['WB_ADDONS']['dbconnect_le'] = array('VERSION' => '0.69', 'OPERATOR' => '>=');
+if (!defined('CAT_VERSION')) {
+    $PRECHECK['WB_ADDONS']['dwoo'] = array('VERSION' => '0.13', 'OPERATOR' => '>=');
+}
 
 global $database;
 $sql = "SELECT * FROM ".TABLE_PREFIX."settings WHERE name='default_charset'";
 $result = $database->query($sql);
 if ($result) {
-	$data = $result->fetchRow();
-	($data['value'] == 'utf-8') ? $status = true : $status = false;
-	$PRECHECK['CUSTOM_CHECKS'] = array(
+    $data = $result->fetchRow();
+    ($data['value'] == 'utf-8') ? $status = true : $status = false;
+    $PRECHECK['CUSTOM_CHECKS'] = array(
     'Default Charset' => array('REQUIRED' => 'utf-8', 'ACTUAL' => $data['value'], 'STATUS' => $status));
 }
 
